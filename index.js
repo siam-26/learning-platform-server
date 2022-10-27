473eb75f9de5b0089e4643f623d6fe9d237bb83a
siam-26 <siamshahriar390@gmail.com>
2022-10-27 10:47:53 +0600

    send res data of course-info(id)

--- index.js
+++ index.js
@@ -3,7 +3,20 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+const coursesInfo = require('./data/coursesInfo.json');
+
 app.use(cors());
+
+app.get('/course-info', (req, res) => {
+    const id = parseInt(req.id);
+    res.send(coursesInfo);
+})
+
+app.get('/course-info/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const selectedCourse = coursesInfo.find(course => course.id === id);
+    res.send(selectedCourse);
+})
 
 app.get('/', (req, res) => {
     res.send('coders-info-bd is running on server')
